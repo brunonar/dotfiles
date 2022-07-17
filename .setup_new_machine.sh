65486e4d7c6341c31b88435fe40db44420f95972
Bruno <brunonar@gmail.com>
2022-07-17 01:03:42 -0300

    Updated .setup_new_machine with new github ssh login method and updated npm global command. Added instructions to update nvim

--- .setup_new_machine.sh
+++ .setup_new_machine.sh
@@ -1,5 +1,5 @@
 echo "Configurando a máquina pro Bruno usar. Isso vai dar muito errado."
-echo "você já deveria ter feito: git clone --bare https://github.com/brunonar/dotfiles.git $HOME/.dotfiles/"
+echo "você já deveria ter feito: git clone --bare git@github.com:brunonar/dotfiles.git $HOME/.dotfiles/"
 alias gdot='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 gdot config --local status.showUntrackedFiles no
 
@@ -8,8 +8,9 @@ gdot checkout
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt update
 sudo apt install nodejs npm unzip fzf neovim
-sudo npm install -g npm
-sudo npm install -g typescript typescript-language-server eslint_d prettier
+sudo npm install --location=global npm
+sudo npm install --location=global typescript typescript-language-server eslint_d prettier
 
 git clone --depth=1 https://github.com/savq/paq-nvim.git     "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+echo "Agora, entre no nvim e instale os plugins com PaqInstall"
 
